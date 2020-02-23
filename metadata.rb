1065a6b1c71736f87bc81a23154901b17654f838
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2020-02-23 14:46:17 +0400

    Add email address validator settings

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Alexander Pyatkin'
 maintainer_email 'aspyatkin@gmail.com'
 license 'MIT'
 description 'Installs and configures VolgaCTF Qualifier platform'
-version '2.0.0'
+version '2.0.1'
 
 depends 'nodejs', '~> 6.0.0'
 depends 'ngx'
